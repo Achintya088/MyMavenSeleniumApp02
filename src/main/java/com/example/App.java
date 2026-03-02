72cddb2510aea3fb7f8852e65ed9b29977c1269f
Achintya088 <itagiachintya7@gmail.com>
2026-03-02 07:29:54 +0000

    second commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+    public static void main(String[] args){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/products");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("search_product")).sendKeys("shirt");
+        driver.findElement(By.id("submit_search")).click();s
+    }
+}
